947077b9607b1efbbd461da7b7bf8948e0a27e1a
Velpistikhe <velpistikhe@gmail.com>
2025-08-27 20:50:56 +0700

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.listen(process.env.APP_PORT, () =>
+  console.log(`Server is running on http://localhost:${process.env.APP_PORT}`)
+);
