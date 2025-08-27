f59f07c7cc87a28dd138ba9c316dcd59e579c35f
Velpistikhe <velpistikhe@gmail.com>
2025-08-27 21:05:10 +0700

    ADD USER ROUTE

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const user = require("./modules/user/user.routes");
 
 dotenv.config();
 
 const app = express();
+
+app.use(user);
 
 app.listen(process.env.APP_PORT, () =>
   console.log(`Server is running on http://localhost:${process.env.APP_PORT}`)
