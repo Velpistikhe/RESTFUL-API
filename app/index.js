4e8acb271887be5b8a31175088805c0abfeabb16
Velpistikhe <velpistikhe@gmail.com>
2025-09-09 18:10:26 +0700

    merubah cors

--- app/index.js
+++ app/index.js
@@ -22,10 +22,17 @@ app.use(
     credentials: true,
   })
 );
+app.use(
+  "/pictures",
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(helmet());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cookieParser());
 
